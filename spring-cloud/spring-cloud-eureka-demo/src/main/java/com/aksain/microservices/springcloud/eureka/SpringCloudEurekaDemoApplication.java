bea9e81afdef42d7d98e13a43461566b68fda067
Amit Kumar <aksain@gmail.com>
2016-10-07 11:07:10 +0530

    Added spring cloud eureka project skeleton.

---
+++ spring-cloud/spring-cloud-eureka-demo/src/main/java/com/aksain/microservices/springcloud/eureka/SpringCloudEurekaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.aksain.microservices.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaDemoApplication.class, args);
+	}
+}
