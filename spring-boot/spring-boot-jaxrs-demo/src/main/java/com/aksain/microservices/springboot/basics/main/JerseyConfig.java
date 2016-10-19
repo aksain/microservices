e1e58145360882512e15b646eb91259c5727daa0
Amit Kumar <kumar.amit@naukri.com>
2016-10-19 10:09:45 +0530

    Moved to latest version of HK2

--- spring-boot/spring-boot-jaxrs-demo/src/main/java/com/aksain/microservices/springboot/basics/main/JerseyConfig.java
+++ spring-boot/spring-boot-jaxrs-demo/src/main/java/com/aksain/microservices/springboot/basics/main/JerseyConfig.java
@@ -3,16 +3,12 @@ package com.aksain.microservices.springboot.basics.main;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
-import com.aksain.microservices.springboot.basics.resources.TutorialResource;
-
 /**
- * @author amit-kumar
- *
+ * @author Amit Kumar
  */
 @Component
 public class JerseyConfig extends ResourceConfig{
 	public JerseyConfig() {
-		register(TutorialResource.class);
 		packages("com.aksain.microservices.springboot.basics.resources");
 	}
 }
