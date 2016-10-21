3579ddf5d7a119b36786a43b68b56465a838ad28
Amit Kumar <kumar.amit@naukri.com>
2016-10-21 09:24:20 +0530

    Replaced package registration with resource registration.

--- spring-cloud/service-tracing/zipkin/author-services/src/main/java/com/aksain/msa/author/JerseyConfig.java
+++ spring-cloud/service-tracing/zipkin/author-services/src/main/java/com/aksain/msa/author/JerseyConfig.java
@@ -3,9 +3,12 @@ package com.aksain.msa.author;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
+import com.aksain.msa.author.domainobjects.Author;
+import com.aksain.msa.author.resources.AuthorResource;
+
 @Component
 public class JerseyConfig extends ResourceConfig {
 	public JerseyConfig() {
-		this.packages("com.aksain.msa.author.resources");
+		this.register(AuthorResource.class);
 	}
 }
