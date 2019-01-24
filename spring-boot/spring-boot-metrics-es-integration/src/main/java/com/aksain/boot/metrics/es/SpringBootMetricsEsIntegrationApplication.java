ae0651d5b8d2ad961902283c4fb743d19f443db0
Amit Kumar <kumar.amit@naukri.com>
2019-01-24 08:26:36 +0530

    Added MeterRegistry configurer for adding gloabl tags

--- spring-boot/spring-boot-metrics-es-integration/src/main/java/com/aksain/boot/metrics/es/SpringBootMetricsEsIntegrationApplication.java
+++ spring-boot/spring-boot-metrics-es-integration/src/main/java/com/aksain/boot/metrics/es/SpringBootMetricsEsIntegrationApplication.java
@@ -1,10 +1,24 @@
 package com.aksain.boot.metrics.es;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 public class SpringBootMetricsEsIntegrationApplication {
+	
+	@Value("${spring.application.name}")
+	private String applicationName;
+	
+	
+	@Bean
+    public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+		return registry -> registry.config().commonTags("application.name", applicationName);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMetricsEsIntegrationApplication.class, args);
