2d4453a94290904ecf83d53b207b269511d20ef4
Amit Kumar <amit.kumar@saintech.co.in>
2019-01-25 10:19:34 +0530

    Added service port to common tags of MeterRegistry

--- spring-boot/spring-boot-metrics-es-integration/src/main/java/com/aksain/boot/metrics/es/ApplicationNameMetricsExtension.java
+++ spring-boot/spring-boot-metrics-es-integration/src/main/java/com/aksain/boot/metrics/es/ApplicationNameMetricsExtension.java
@@ -15,13 +15,18 @@ public class ApplicationNameMetricsExtension {
 	@Value("${spring.application.name}")
 	private String applicationName;
 	
+	@Value("${server.port:8080}")
+	private String serverPort;
+	
 	@Bean
     public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() throws UnknownHostException {
 		final String hostName = InetAddress.getLocalHost().getHostName();
 		return registry -> registry.config()
+				// Add service name, host and port to global tags
 				.commonTags(
 						"service.name", applicationName, 
-						"service.host", hostName
+						"service.host", hostName,
+						"service.port", serverPort
 					);
 	}
 }
